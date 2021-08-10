6789870491cc5308a7cebd8efc81776645e3d6fe
Dylan Silke <d.ylan.s.silke@gmail.com>
2021-08-10 11:32:47 +1000

    Adds custom configuration for creating SessionStores

--- dual_sessions/middleware.py
+++ dual_sessions/middleware.py
@@ -8,6 +8,8 @@ from django.contrib.sessions.middleware import SessionMiddleware
 from django.contrib.sessions.backends.base import SessionBase
 from django.conf import settings
 
+from typing import Iterable
+
 
 class DualSessionMiddleware(SessionMiddleware):
     def __init__(self, get_response):
@@ -24,12 +26,29 @@ class DualSessionMiddleware(SessionMiddleware):
         engine = import_module(settings.UNUTH_SESSION_ENGINE)
         return engine.SessionStore
 
+    def create_store(self, SessionStore, session_key, *args):
+        return SessionStore(session_key, *args)
+
+    def get_auth_args(self) -> Iterable:
+        """
+        Get arguments for creating the authenticated SessionStore object
+        Note: this is only required if you are using a custom Session backend
+         """
+        raise NotImplemented
+
+    def get_unauth_args(self) -> Iterable:
+        """
+        Get arguments for creating the unauthenticated SessionStore object
+        Note: this is only required if you are using a custom Session backend
+        """
+        raise NotImplemented
+
     def process_request(self, request):
         # Get the session key
         session_key = request.COOKIES.get(settings.SESSION_COOKIE_NAME)
         if request.user.is_authenticated:
             SessionStore = self.get_auth_store()
-            request.session = SessionStore(session_key)
+            request.session = self.create_store(SessionStore, session_key, *self.get_auth_args())
         else:
             SessionStore = self.get_unauth_store()
-            request.session = SessionStore(session_key)
+            request.session = self.create_store(SessionStore, session_key, *self.get_unauth_args())
